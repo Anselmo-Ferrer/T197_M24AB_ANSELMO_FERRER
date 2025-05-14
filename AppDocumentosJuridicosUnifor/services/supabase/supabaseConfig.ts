a091c127ac09cd0f58918d1cfe75ac65cee236e6
Anselmo Dev <anselmoferrer.dev@gmail.com>
2025-05-14 18:13:50 -0300

    att project

--- AppDocumentosJuridicosUnifor/services/supabase/supabaseConfig.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import {
-  SUPABASE_URL_DOCUMENTS,
-  SUPABASE_KEY_DOCUMENTS
-} from '@env';
-
-export const supabase = createClient(SUPABASE_URL_DOCUMENTS, SUPABASE_KEY_DOCUMENTS);
